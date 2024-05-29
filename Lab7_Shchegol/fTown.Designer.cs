27740401a9f6b6fd554056db97a2bd1106fdd1e6
VikaShchegol <134399555+VikaShchegol@users.noreply.github.com>
2024-05-29 10:22:12 +0300

    Добавьте файлы проекта.

---
+++ Lab7_Shchegol/fTown.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace Lab7_Shchegol
+{
+        partial class fTown
+        {
+            private System.ComponentModel.IContainer components = null;
+
+            protected override void Dispose(bool disposing)
+            {
+                if (disposing && (components != null))
+                {
+                    components.Dispose();
+                }
+                base.Dispose(disposing);
+            }
+
+            #region Код, автоматически созданный конструктором форм Windows
+
+            private void InitializeComponent()
+            {
+            this.lblGeneralInfo = new System.Windows.Forms.Label();
+            this.lblCityName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblCountry = new System.Windows.Forms.Label();
+            this.txtCountry = new System.Windows.Forms.TextBox();
+            this.lblRegion = new System.Windows.Forms.Label();
+            this.txtRegion = new System.Windows.Forms.TextBox();
+            this.lblPopulation = new System.Windows.Forms.Label();
+            this.txtPopulation = new System.Windows.Forms.TextBox();
+            this.lblAnnualIncome = new System.Windows.Forms.Label();
+            this.txtYearIncome = new System.Windows.Forms.TextBox();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.txtSquare = new System.Windows.Forms.TextBox();
+            this.lblTransport = new System.Windows.Forms.Label();
+            this.chkHasPort = new System.Windows.Forms.CheckBox();
+            this.chkHasAirport = new System.Windows.Forms.CheckBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblGeneralInfo
+            // 
+            this.lblGeneralInfo.AutoSize = true;
+            this.lblGeneralInfo.BackColor = System.Drawing.Color.Transparent;
+            this.lblGeneralInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.lblGeneralInfo.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.lblGeneralInfo.Location = new System.Drawing.Point(4, 10);
+            this.lblGeneralInfo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblGeneralInfo.Name = "lblGeneralInfo";
+            this.lblGeneralInfo.Size = new System.Drawing.Size(94, 16);
+            this.lblGeneralInfo.TabIndex = 0;
+            this.lblGeneralInfo.Text = "Загальні дані";
+            // 
+            // lblCityName
+            // 
+            this.lblCityName.AutoSize = true;
+            this.lblCityName.Location = new System.Drawing.Point(14, 40);
+            this.lblCityName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCityName.Name = "lblCityName";
+            this.lblCityName.Size = new System.Drawing.Size(86, 16);
+            this.lblCityName.TabIndex = 1;
+            this.lblCityName.Text = "Назва міста";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(152, 37);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(126, 21);
+            this.txtName.TabIndex = 2;
+            // 
+            // lblCountry
+            // 
+            this.lblCountry.AutoSize = true;
+            this.lblCountry.Location = new System.Drawing.Point(14, 70);
+            this.lblCountry.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCountry.Name = "lblCountry";
+            this.lblCountry.Size = new System.Drawing.Size(50, 16);
+            this.lblCountry.TabIndex = 3;
+            this.lblCountry.Text = "Країна";
+            // 
+            // txtCountry
+            // 
+            this.txtCountry.Location = new System.Drawing.Point(155, 67);
+            this.txtCountry.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtCountry.Name = "txtCountry";
+            this.txtCountry.Size = new System.Drawing.Size(126, 21);
+            this.txtCountry.TabIndex = 4;
+            // 
+            // lblRegion
+            // 
+            this.lblRegion.AutoSize = true;
+            this.lblRegion.Location = new System.Drawing.Point(14, 100);
+            this.lblRegion.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblRegion.Name = "lblRegion";
+            this.lblRegion.Size = new System.Drawing.Size(49, 16);
+            this.lblRegion.TabIndex = 5;
+            this.lblRegion.Text = "Регіон";
+            // 
+            // txtRegion
+            // 
+            this.txtRegion.Location = new System.Drawing.Point(155, 97);
+            this.txtRegion.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtRegion.Name = "txtRegion";
+            this.txtRegion.Size = new System.Drawing.Size(126, 21);
+            this.txtRegion.TabIndex = 6;
+            // 
+            // lblPopulation
+            // 
+            this.lblPopulation.AutoSize = true;
+            this.lblPopulation.Location = new System.Drawing.Point(14, 130);
+            this.lblPopulation.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPopulation.Name = "lblPopulation";
+            this.lblPopulation.Size = new System.Drawing.Size(135, 16);
+            this.lblPopulation.TabIndex = 7;
+            this.lblPopulation.Text = "Кількість мешканців";
+            // 
+            // txtPopulation
+            // 
+            this.txtPopulation.Location = new System.Drawing.Point(155, 127);
+            this.txtPopulation.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtPopulation.Name = "txtPopulation";
+            this.txtPopulation.Size = new System.Drawing.Size(126, 21);
+            this.txtPopulation.TabIndex = 8;
+            // 
+            // lblAnnualIncome
+            // 
+            this.lblAnnualIncome.AutoSize = true;
+            this.lblAnnualIncome.Location = new System.Drawing.Point(14, 160);
+            this.lblAnnualIncome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblAnnualIncome.Name = "lblAnnualIncome";
+            this.lblAnnualIncome.Size = new System.Drawing.Size(115, 16);
+            this.lblAnnualIncome.TabIndex = 9;
+            this.lblAnnualIncome.Text = "Річний дохід, грн";
+            // 
+            // txtYearIncome
+            // 
+            this.txtYearIncome.Location = new System.Drawing.Point(155, 157);
+            this.txtYearIncome.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtYearIncome.Name = "txtYearIncome";
+            this.txtYearIncome.Size = new System.Drawing.Size(126, 21);
+            this.txtYearIncome.TabIndex = 10;
+            // 
+            // lblArea
+            // 
+            this.lblArea.AutoSize = true;
+            this.lblArea.Location = new System.Drawing.Point(14, 190);
+            this.lblArea.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(90, 16);
+            this.lblArea.TabIndex = 11;
+            this.lblArea.Text = "Площа, кв.км";
+            // 
+            // txtSquare
+            // 
+            this.txtSquare.Location = new System.Drawing.Point(155, 187);
+            this.txtSquare.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtSquare.Name = "txtSquare";
+            this.txtSquare.Size = new System.Drawing.Size(126, 21);
+            this.txtSquare.TabIndex = 12;
+            // 
+            // lblTransport
+            // 
+            this.lblTransport.AutoSize = true;
+            this.lblTransport.BackColor = System.Drawing.Color.Transparent;
+            this.lblTransport.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.lblTransport.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.lblTransport.Location = new System.Drawing.Point(4, 225);
+            this.lblTransport.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTransport.Name = "lblTransport";
+            this.lblTransport.Size = new System.Drawing.Size(78, 16);
+            this.lblTransport.TabIndex = 13;
+            this.lblTransport.Text = "Транспорт";
+            // 
+            // chkHasPort
+            // 
+            this.chkHasPort.AutoSize = true;
+            this.chkHasPort.Location = new System.Drawing.Point(16, 247);
+            this.chkHasPort.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.chkHasPort.Name = "chkHasPort";
+            this.chkHasPort.Size = new System.Drawing.Size(124, 20);
+            this.chkHasPort.TabIndex = 14;
+            this.chkHasPort.Text = "Місто має порт";
+            this.chkHasPort.UseVisualStyleBackColor = true;
+            // 
+            // chkHasAirport
+            // 
+            this.chkHasAirport.AutoSize = true;
+            this.chkHasAirport.Location = new System.Drawing.Point(16, 273);
+            this.chkHasAirport.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.chkHasAirport.Name = "chkHasAirport";
+            this.chkHasAirport.Size = new System.Drawing.Size(156, 20);
+            this.chkHasAirport.TabIndex = 15;
+            this.chkHasAirport.Text = "Місто має аеропорт";
+            this.chkHasAirport.UseVisualStyleBackColor = true;
+            // 
+            // btnOK
+            // 
+            this.btnOK.BackColor = System.Drawing.Color.Honeydew;
+            this.btnOK.Location = new System.Drawing.Point(193, 266);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(88, 27);
+            this.btnOK.TabIndex = 16;
+            this.btnOK.Text = "Oк";
+            this.btnOK.UseVisualStyleBackColor = false;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnCancel.Location = new System.Drawing.Point(193, 225);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(88, 27);
+            this.btnCancel.TabIndex = 17;
+            this.btnCancel.Text = "Скасувати";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // fTown
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(314, 303);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.chkHasAirport);
+            this.Controls.Add(this.chkHasPort);
+            this.Controls.Add(this.lblTransport);
+            this.Controls.Add(this.txtSquare);
+            this.Controls.Add(this.lblArea);
+            this.Controls.Add(this.txtYearIncome);
+            this.Controls.Add(this.lblAnnualIncome);
+            this.Controls.Add(this.txtPopulation);
+            this.Controls.Add(this.lblPopulation);
+            this.Controls.Add(this.txtRegion);
+            this.Controls.Add(this.lblRegion);
+            this.Controls.Add(this.txtCountry);
+            this.Controls.Add(this.lblCountry);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblCityName);
+            this.Controls.Add(this.lblGeneralInfo);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "fTown";
+            this.Text = "Додавання";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+            }
+
+            #endregion
+
+            private System.Windows.Forms.Label lblGeneralInfo;
+            private System.Windows.Forms.Label lblCityName;
+            private System.Windows.Forms.TextBox txtName;
+            private System.Windows.Forms.Label lblCountry;
+            private System.Windows.Forms.TextBox txtCountry;
+            private System.Windows.Forms.Label lblRegion;
+            private System.Windows.Forms.TextBox txtRegion;
+            private System.Windows.Forms.Label lblPopulation;
+            private System.Windows.Forms.TextBox txtPopulation;
+            private System.Windows.Forms.Label lblAnnualIncome;
+            private System.Windows.Forms.TextBox txtYearIncome;
+            private System.Windows.Forms.Label lblArea;
+            private System.Windows.Forms.TextBox txtSquare;
+            private System.Windows.Forms.Label lblTransport;
+            private System.Windows.Forms.CheckBox chkHasPort;
+            private System.Windows.Forms.CheckBox chkHasAirport;
+            private System.Windows.Forms.Button btnOK;
+            private System.Windows.Forms.Button btnCancel;
+        }
+    }
---
+++ Lab7_Shchegol/fTown.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.MonthCalendar;
+
+namespace Lab7_Shchegol
+{
+    public partial class fTown : Form
+    {
+        private Town _town;
+        public fTown(ref Town town)
+        {
+            InitializeComponent();
+            _town = town;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtName.Text) ||
+        string.IsNullOrWhiteSpace(txtCountry.Text) ||
+        string.IsNullOrWhiteSpace(txtRegion.Text) ||
+        !int.TryParse(txtPopulation.Text, out int population) ||
+        !double.TryParse(txtYearIncome.Text, out double yearIncome) ||
+        !double.TryParse(txtSquare.Text, out double square))
+            {
+                MessageBox.Show("Будь ласка, заповніть всі поля коректно.");
+                return;
+            }
+
+            _town.Name = txtName.Text;
+            _town.Country = txtCountry.Text;
+            _town.Region = txtRegion.Text;
+            _town.Population = population;
+            _town.YearIncome = yearIncome;
+            _town.Square = square;
+            _town.HasPort = chkHasPort.Checked;
+            _town.HasAirport = chkHasAirport.Checked;
+
+            this.DialogResult = DialogResult.OK;
+        }
+
+    }
+}
